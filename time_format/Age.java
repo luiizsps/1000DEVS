69d8682bec2250b03fb25eb5850e80bcd0b68b7e
luiizsps <lsps1019@gmail.com>
2025-11-20 23:07:26 -0300

    fix current date issue

--- time_format/Age.java
+++ time_format/Age.java
@@ -1,3 +1,5 @@
+package time_format;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
@@ -6,21 +8,21 @@ public class Age {
 	public static void main(String[] args) {
 		LocalDate currentDate = LocalDate.now();
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		String birthDateString = "10/06/2002";
+		String birthDateString = "10/12/2002";
 		LocalDate nextBirthday = null;
 
 		LocalDate birthDate = LocalDate.parse(birthDateString, dateFormatter);
 		Period period = Period.between(birthDate, currentDate);
 		
-		System.out.printf("O usuário tem %d anos %d meses e %d dias.", period.getYears(), period.getMonths(), period.getDays());
+		System.out.printf("O usuário tem %d anos %d meses e %d dias.\n", period.getYears(), period.getMonths(), period.getDays());
 
-		LocalDate birthdayCurrentYear =  LocalDate.of(currentDate.getYear(), birthDate.getDayOfMonth(), birthDate.getMonthValue());
+		LocalDate birthdayCurrentYear =  LocalDate.of(currentDate.getYear(), birthDate.getMonthValue(), birthDate.getDayOfMonth());
 		
 		if(currentDate.isAfter(birthdayCurrentYear)) {
 			nextBirthday = birthdayCurrentYear.plusYears(1);
 		} else {
 			nextBirthday = birthdayCurrentYear;
 		}
-		System.out.printf("O seu próximo aniversário é "+ nextBirthday.format(dateFormatter));
+		System.out.printf("O seu próximo aniversário é "+ nextBirthday.format(dateFormatter)+"\n");
 	}
 }
--- time_format/DifDate.java
+++ time_format/DifDate.java
@@ -8,14 +8,26 @@ public class DifDate {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         LocalDateTime firstDate = LocalDateTime.parse("01/01/2024 08:00:00", dateFormatter);
         LocalDateTime secondDate = LocalDateTime.parse("10/02/2025 10:00:00", dateFormatter);
+        LocalDateTime currentDate = LocalDateTime.now();
 
         Duration timeBetween = Duration.between(firstDate, secondDate);
+        Duration timeBetwennSecondDate = Duration.between(secondDate, currentDate);
+        Duration timeBetwennFirstDate = Duration.between(firstDate, currentDate);
 
         long seconds = timeBetween.getSeconds();
         double minutes = seconds / 60;
         double hours = minutes / 60;
         double days = hours / 24;
 
+        long secondSecondDateGap = timeBetwennSecondDate.getSeconds();
+        long firstDateGap = timeBetwennFirstDate.getSeconds();
+
         System.out.printf("Dias: %.0f\nHoras: %.0f\nMinutos: %.0f\nSegundos: %d\n", days, hours, minutes, seconds);
+
+        if (secondSecondDateGap > firstDateGap) {
+            System.out.println("A primeira data é a mais recente.");
+        } else {
+            System.out.println("A segunda data é a mais recente");
+        }
     }
 }
